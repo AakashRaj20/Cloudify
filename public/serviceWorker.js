a29b8542db1ad957dfb546e7d991912294a486ee
Aakash Raj <aakashraj914@gmail.com>
2023-09-24 20:53:01 +0530

    fixed service worker file

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,44 +1,25 @@
-var CACHE_NAME = "pwa-task-manager";
-var urlsToCache = ["/", "/completed"];
+const CACHE_NAME = "your-app-cache";
+const urlsToCache = [
+  "/",
+  "/index.html",
+];
 
-var self = this;
+const self = this;
 
-// Install a service worker
+// Install service worker
 self.addEventListener("install", (event) => {
-  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Cache and return requests
+// Fetch from cache or network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-// Update a service worker
-self.addEventListener("activate", (event) => {
-  var cacheWhitelist = ["pwa-task-manager"];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
