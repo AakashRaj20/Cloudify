29be419c9dde4e61f2f0130c176ff96c9e948fde
Aakash Raj <aakashraj914@gmail.com>
2023-09-24 20:34:29 +0530

    added PWA to the project

--- src/index.js
+++ src/index.js
@@ -15,6 +15,22 @@ root.render(
     </Provider>
 );
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
